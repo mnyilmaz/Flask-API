45d2bf0ef47082d90770fc52d1e0bca470601464
mnyilmaz <mervenur.yilmazz2326@gmail.com>
2023-07-21 01:43:42 +0300

    Set FlaskAPI

---
+++ README.md
@@ -0,0 +1,13 @@
+<div align="center">
+<img src="https://png2.cleanpng.com/sh/20f1b5da1264b11b8915eb1ca3be2c9f/L0KzQYm3VsEyN5J1kpH0aYP2gLBuTfFxeJ1ue9N9aXBxPcH5jBdzaZ5ygdDwLXnxhLb5hvFkbV53feJ7ZYPofsXolPlwdpJxRadrMXazR4rohfI5O2E8RqM8NUO5QYq7UcUzQGg7SKQ6OEm5RIi1kP5o/kisspng-application-programming-interface-representational-5b1f079aeb8307.1353619415287602189647.png" width="300" height="300">
+  
+</div>
+
+<h1 align="center">Flask API Development </h1>
+
+*  [:hash: Purpose](#hash-amaç)
+
+# :hash: Purpose
+<p align="justify">
+Flask API is an API built on the Flask web framework, which serves as an interface for software applications to communicate. It receives and processes web requests, generating responses for specific URLs. APIs enable different systems or applications to share data, often providing data in formats like JSON. Flask API can be used in various fields, facilitating data exchange between applications. With Flask, the task at hand is to create a backend that allows our frontend to communicate with the Python application developed by the Chatbot team. This Flask application will handle GET and POST API requests, enabling the transmission of messages from the website to the chatbot, and the chatbot's responses will be sent back to the website via the API.
+</br>
---
+++ chatbot/chatbot.py
@@ -0,0 +1,79 @@
+import random
+import json
+import pickle
+import numpy as np
+import tensorflow as tf
+import os
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+intents_file_path = os.path.join(current_dir, 'intents.json')
+words_file_path = os.path.join(current_dir, 'words.pkl')
+classes_file_path = os.path.join(current_dir, 'classes.pkl')
+model_file_path = os.path.join(current_dir, 'chatbotmodel.h5')
+
+lemmatizer = WordNetLemmatizer()
+with open(intents_file_path, 'r') as intentFile:
+    intents = json.load(intentFile)
+with open(words_file_path, 'rb') as wordFile:
+    words = pickle.load(wordFile)
+with open(classes_file_path, 'rb') as classesFile:
+    classes = pickle.load(classesFile)
+model = tf.keras.models.load_model(model_file_path)
+
+
+# Cleaning up the sentence func
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+
+# Indicate if there is word or not with full of list 1 and 0s
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+            # otherwise its automatically zero
+    return np.array(bag)
+
+
+# Predict func
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+
+# For chatting with Chatbot
+def get_repsonse(intents_list, intent_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intent_json['intents']
+
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+print('Chatbot is ready to talk:\n')
+
+while True:
+    message = input("")
+    ints = predict_class(message)
+    res = get_repsonse(ints, intents)
+    print(res)
